1f9a17829fd2c73b58caae1f83280b638ac98dae
olgakahraman <ol.khrmn@gmail.com>
2024-07-16 21:52:17 -0400

    created a header with logo and added basic styling to it; imported header into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import Header from './components/Header'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
 
 function App() {
-  
-
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  )
+	return (
+		<div className="container">
+			<Header />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React from 'react';
 
-
-export default function Header(){
-  return(
-    <header className="header">
-      <img src="./images/logo.png" alt="logo" className="header-image" />
-      <h2 className="header-title">Meme Generator</h2>
-      <h4 className="header-project">React Course - Project 3</h4>
-    </header>
-  )
+export default function Header() {
+	return (
+		<header className="header">
+			<img
+				src="./images/logo.png"
+				alt="logo"
+				className="header-image"
+			/>
+			<h2 className="header-title">Meme Generator</h2>
+			<h4 className="header-project">
+				React Course - Project 3
+			</h4>
+		</header>
+	);
 }
